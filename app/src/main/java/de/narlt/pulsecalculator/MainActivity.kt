30fcffe7179bcfdfcc491c0121e7bdc7d9d205c1
Niklas Arlt <n.a@live.de>
2024-05-17 22:10:37 +0200

    Complete rewrite with jetpack compose

--- app/src/main/java/de/narlt/pulsecalculator/MainActivity.kt
+++ app/src/main/java/de/narlt/pulsecalculator/MainActivity.kt
@@ -1,28 +1,62 @@
 package de.narlt.pulsecalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import de.narlt.pulsecalculator.ui.theme.PulsecalculatorTheme
+import java.util.LinkedList
 import kotlin.math.roundToLong
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : ComponentActivity() {
     private var bpm = 0L
     private var hasBpm = false
     private var lastClicks = LinkedList<Long>()
     private val minClicksForCalculation = 4
     private val resetTime = 1500
+    private var buttonText by mutableStateOf("--- bpm")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        cContent.setOnClickListener(this)
-        resetText()
+        enableEdgeToEdge()
+        setContent {
+            PulsecalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PulseButton(
+                        onClick = this::buttonClicked,
+                        textRef = buttonText,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
     }
 
-    override fun onClick(v: View?) {
+    private fun buttonClicked() {
         if (lastClicks.size > 0 && SystemClock.elapsedRealtime() - lastClicks.last > resetTime) {
             resetText()
             lastClicks.clear()
@@ -37,11 +71,54 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val timePerBeat = elapsed / (lastClicks.size - 1)
             bpm = (60000.0 / timePerBeat).roundToLong()
             hasBpm = true
-            tvBPM.text = "$bpm bpm"
+            buttonText = "$bpm bpm"
         }
     }
 
     private fun resetText() {
-        tvBPM.text = "--- bpm"
+        buttonText = "--- bpm"
+    }
+}
+
+@Composable
+fun PulseButton(onClick: () -> Unit, textRef: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(20.dp),
+        modifier = modifier
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .weight(.9f)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = textRef,
+                    fontSize = TextUnit(30f, TextUnitType.Sp),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+
+            Row(modifier = Modifier.weight(.1f)) {
+                Text(
+                    text = "(Click the screen in the rhythm of your heart beat)",
+                    modifier = Modifier.padding(top = 40.dp)
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PulseButtonPreview() {
+    PulsecalculatorTheme {
+        PulseButton({}, "text", Modifier.padding(Dp(20f)))
     }
 }
